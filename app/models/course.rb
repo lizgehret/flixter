ccec929c05e24148e5a3534e08d16963b2f00c4c
Liz Gehret <elizabeth.gehret@gmail.com>
2019-10-10 03:58:14 +0000

    Adding course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  #title validation - needs to exist
+  validates :title, presence: true
+  validates :description, presence: true
+
+  #cost validation - can't be a negative number
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
