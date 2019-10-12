278858da9f85e284f604d8ba9ef15f2b7155bcbc
Liz Gehret <elizabeth.gehret@gmail.com>
2019-10-12 20:24:56 +0000

    Adding Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   #hooking up courses for instructors - namespace b/c of separate views/permissions
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end    
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
